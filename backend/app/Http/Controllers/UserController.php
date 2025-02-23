99a8c3019a470f53dbb2a3b45f0857bbeb16208b
Hoanqia <nhg2k3@gmail.com>
2025-02-23 12:02:08 +0700

    Updated backend

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -113,5 +113,69 @@ class UserController extends Controller
 }
 
    
+    public function update(Request $request){
+        $user = auth('sanctum')->user();
     
+        if (!$user) {
+            return response()->json(['message' => 'Bạn chưa đăng nhập!','status' => 'error'], 401);
+        }
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:500',
+            'phone' =>  'required|string|max:10',
+        ]);
+        try {
+            $user->update($validatedData);
+            $user->makeHidden(['password','role']);
+            return response()->json([
+                'message' => 'Cập nhật thông tin cá nhân thành công',
+                'status' => 'success',
+                'data' => $user,
+            ],200);
+        }catch(\Exception $e){
+            return response()->json([
+                'message' => 'Cập nhật thông tin cá nhân thất bại',
+                'status' => 'error',
+            ],500);
+        }
+    }
+    public function changePassword(Request $request){
+        $user = auth('sanctum')->user();
+    
+        if (!$user) {
+            return response()->json(['message' => 'Bạn chưa đăng nhập!','status' => 'error'], 401);
+        }
+        if (!Hash::check($request->old_password, $user->password)) {
+            return response()->json([
+                'message' => 'Mật khẩu cũ không đúng',
+                'status' => 'error'
+            ], 400);
+        }
+        if (!$request->has('new_password_confirmation')) {
+            return response()->json([
+                'message' => 'Bạn cần nhập lại mật khẩu để xác nhận.',
+                'status' => 'error'
+            ], 400);
+        }
+        $validatedData = $request->validate([
+            'new_password' => 'required|string|min:6|confirmed',
+        ]);
+        $hashedPassword = Hash::make($validatedData['new_password']);
+        try {
+            $user->update([
+                'password' => $hashedPassword,
+            ]);
+    
+            return response()->json([
+                'message' => 'Thay đổi mật khẩu thành công',
+                'status' => 'success'
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Thay đổi mật khẩu thất bại',
+                'status' => 'error',
+                'error' => $e->getMessage() 
+            ], 500);
+        }
+
+    }
 }
